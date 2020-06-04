075a1afd1450c02aaabd96f2d80cbd98a504922e
Pascal <pascal-0x90@protonmail.com>
2020-06-04 08:58:01 -0700

    Added timeout option for CNAME analysis.

--- src/dyFront/frontingEngine/detectCDN/cdn_check.py
+++ src/dyFront/frontingEngine/detectCDN/cdn_check.py
@@ -14,7 +14,7 @@ import urllib.request as request
 
 # Third-Party Libraries
 import censys.websites as censysLookup
-from dns.resolver import NXDOMAIN, NoAnswer, NoNameservers, query
+from dns.resolver import NXDOMAIN, NoAnswer, NoNameservers, Resolver, query, Timeout
 from ipwhois import HTTPLookupError, IPWhois
 
 # Internal Libraries
@@ -79,13 +79,19 @@ class cdnCheck:
 
     def cname(self, dom: Domain) -> int:
         """Collect CNAME records on domain."""
+        resolver = Resolver()
+        resolver.timeout = 10
+        resolver.lifetime = 10
+        cname_query = resolver.query
         try:
-            response = query(dom.url, "cname")
+            response = cname_query(dom.url, "cname")
             dom.cnames = [record.to_text() for record in response]
         except NoAnswer:
             return 1
         except NXDOMAIN:
             return 2
+        except Timeout:
+            return 3
         return 0
 
     def namesrv(self, dom: Domain) -> int:
